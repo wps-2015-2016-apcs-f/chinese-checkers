5933876727a4549ded807901b20fa03c4f2226e3
wps-dpetty <dpetty@winchesterps.org>
2016-06-09 12:46:37 -0400

    Initial add of StrategyTest.java.

---
+++ src/test/java/chinesecheckers/StrategyTest.java
@@ -0,0 +1,34 @@
+/*
+ * StrategyTest.java
+ *
+ * @author 2015-2016 APCS A-Block
+ * @author David C. Petty <dpetty@winchesterps.org>
+ */
+package chinesecheckers;
+
+import org.junit.*;
+
+/**
+ * Unit test for {@link Strategy}.
+ */
+public class StrategyTest {
+    /**
+     * Set up before every test.
+     */
+    @Before
+    public void setUp() {
+    }
+    /**
+     * Tear down after every test.
+     */
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue( true );
+    }
+}
