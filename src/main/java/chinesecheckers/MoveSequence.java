44eebb8ad04cc7679a5317261de3ca838107b6f7
Justin Yu <justinyu1618@gmail.com>
2016-06-13 11:35:55 -0400

    Added comments and empty methods to Strategy, created new move sequence

---
+++ src/main/java/chinesecheckers/MoveSequence.java
@@ -0,0 +1,17 @@
+/*
+ * MoveSequence.java
+ *
+ * @author 2015-2016 APCS F-Block
+ * @author Chris Kang
+ * @author Justin Yu
+ */
+
+package chinesecheckers;
+
+import java.util.ArrayList;
+
+public class MoveSequence {
+	
+//Fields
+	private ArrayList<Hole> moves;
+}
--- src/main/java/chinesecheckers/Strategy.java
+++ src/main/java/chinesecheckers/Strategy.java
@@ -5,8 +5,31 @@
  * @author Chris Kang
  * @author Justin Yu
  */
+
+/*git commit testing*/
+
 package chinesecheckers;
 
 public class Strategy {
 //Fields
+	
+	
+//Methods
+	
+	public Location getBestMove()
+	{
+		int x=0, y=0;
+		return new Hole(x,y);
+	}
+
+	public int getDistance(){
+		
+		return 0;
+	}
+	
+	public void computerMove(){
+		
+	}
+	
+	
 }
