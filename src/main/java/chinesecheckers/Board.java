0b587e1900f9b8d28b777676e4caa3d7baeaac36
SHARON XIANG <sharonxiang777@gmail.com>
2016-06-14 13:26:30 -0400

    Removed extraneous code.

--- src/main/java/chinesecheckers/Board.java
+++ src/main/java/chinesecheckers/Board.java
@@ -61,29 +61,9 @@ public class Board extends JComponent {
     private static final Color R = Color.RED;
     private static final Color P = Color.PINK;
     private static final Color X = Color.MAGENTA;
-
-    // A grid of colors.
-    private Color grid[][] = {
-        { W, W, W, W, W, W, Y, W, W, W, W, W, W, },
-          { W, W, W, W, W, Y, Y, W, W, W, W, W, W, },
-        { W, W, W, W, W, Y, Y, Y, W, W, W, W, W, },
-          { W, W, W, W, Y, Y, Y, Y, W, W, W, W, W, },
-        { K, K, K, K, X, X, X, X, X, G, G, G, G, },
-          { K, K, K, X, X, X, X, X, X, G, G, G, W, },
-        { W, K, K, X, X, X, X, X, X, X, G, G, W, },
-          { W, K, X, X, X, X, X, X, X, X, G, W, W, },
-        { W, W, X, X, X, X, X, X, X, X, X, W, W, },
-          { W, P, X, X, X, X, X, X, X, X, L, W, W, },
-        { W, P, P, X, X, X, X, X, X, X, L, L, W, },
-          { P, P, P, X, X, X, X, X, X, L, L, L, W, },
-        { P, P, P, P, X, X, X, X, X, L, L, L, L, },
-          { W, W, W, W, R, R, R, R, W, W, W, W, W, },
-        { W, W, W, W, W, R, R, R, W, W, W, W, W, },
-          { W, W, W, W, W, R, R, W, W, W, W, W, W, },
-        { W, W, W, W, W, W, R, W, W, W, W, W, W, },
-    };
         
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) 
+    {
         super.paintComponent(g);
 
         int dHole = Math.round(Math.min(getWidth() / H_SPACES, getHeight() / V_SPACES * ROOT3));
@@ -93,16 +73,11 @@ public class Board extends JComponent {
         g.fillRect(0, 0, getWidth(), getHeight());
 
         // draw holes
-        for (int row = 0; row < grid.length * 2; row++) {
-            for (int col = 0; col < grid[row / 2].length * 2; col++) {
-                if (col % 2 != 0 && row % 2 != 0 ) {
-                    int offset = row / 2 % 2 != 0 ? dHole : 0;
-                    g.setColor(grid[row / 2][col / 2]);
-                    if (g.getColor().equals(X))
-                    g.drawOval(col * dHole + offset, Math.round(row * dHole / ROOT3), dHole, dHole);
-                    else
-                    g.fillOval(col * dHole + offset, Math.round(row * dHole / ROOT3), dHole, dHole);
-                }
+        for (int row = 0; row < Grid.SIZE; row++) 
+        {
+            for (int col = 0; col < Grid.SIZE; col++) 
+            {    
+                
             }
         }
     }
