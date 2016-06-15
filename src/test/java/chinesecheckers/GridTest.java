49f1fbc0c4b767da49bc57460339739a03c1acf6
wps-dpetty <dpetty@winchesterps.org>
2016-06-15 18:25:40 -0400

    Updated Grid methods and added *lots* of Javadoc comments. Added testCheck test to GridTest to evaluate isValidMethod -- though that method probably ought not to require from, to to be: Marble, Hole.

--- src/test/java/chinesecheckers/GridTest.java
+++ src/test/java/chinesecheckers/GridTest.java
@@ -14,7 +14,9 @@ import org.junit.*;
 public class GridTest {
     
     private Grid grid;
-    private Location lC, lL, lAL, lAR, lR, lBR, lBL;
+    private Location lC, lL, lAL, lAR, lR, lBR, lBL, lB;
+    private Location[] fromA, toA;
+    private boolean[] boolA;
 
     /**
      * Set up before every test.
@@ -22,13 +24,18 @@ public class GridTest {
     @Before
     public void setUp() {
         grid = new Grid();
-        lC = grid.getLocation(8, 12);
-        lL = grid.getLocation(8, 11);
-        lAL = grid.getLocation(7, 12);
-        lAR = grid.getLocation(7, 13);
-        lR = grid.getLocation(8, 13);
-        lBR = grid.getLocation(9, 12);
-        lBL = grid.getLocation(9, 11);
+        lC = grid.getLocation(8, 12);   // Hole (center of the next six)
+        lL = grid.getLocation(8, 11);   // Hole
+        lAL = grid.getLocation(7, 12);  // Hole
+        lAR = grid.getLocation(7, 13);  // Marble (black)
+        lR = grid.getLocation(8, 13);   // null
+        lBR = grid.getLocation(9, 12);  // Marble (blue)
+        lBL = grid.getLocation(9, 11);  // Hole
+
+        lB = grid.getLocation(7, 13);   // Marble (black)
+        fromA = new Location[] { lAR, lAR, lB, };
+        toA = new Location[] { lAL, lC, lC, };
+        boolA = new boolean[] { true, true, true, };
     }
     /**
      * Tear down after every test.
@@ -38,7 +45,7 @@ public class GridTest {
         grid = null;
     }
     /**
-     * Rigourous Test :-)
+     * Test the 'at' direction methods.
      */
     @Test
     public void testAt() {
@@ -48,5 +55,16 @@ public class GridTest {
         Assert.assertTrue(lR + " != " + grid.atRight(lC), lR == grid.atRight(lC));
         Assert.assertTrue(lBR + " != " + grid.atBelowRight(lC), lBR == grid.atBelowRight(lC));
         Assert.assertTrue(lBL + " != " + grid.atBelowLeft(lC), lBL == grid.atBelowLeft(lC));
+    }
+    /**
+     * Test the 'check' direction methods.
+     */
+    @Test
+    public void testCheck() {
+        // RED_FLAG: isValidMove requires start, land to be: marble, hole.
+        assert fromA.length == toA.length && toA.length == boolA.length;
+        for (int i = 0; i < toA.length; i++)
+            Assert.assertTrue("grid.checkMove(" + fromA[i] + ", " + toA[i] + ") != " + boolA[i],
+                grid.isValidMove(fromA[i], toA[i]) == boolA[i]);
     }
 }
