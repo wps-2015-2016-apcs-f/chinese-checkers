d69db63d2687cfc9486892e2502946d56af7427b
Emilia Lew <emiliaclew@gmail.com>
2016-06-17 14:24:33 -0400

    added getAllMoves method and simplified checkMove method

--- src/main/java/chinesecheckers/MoveSequence.java
+++ src/main/java/chinesecheckers/MoveSequence.java
@@ -11,40 +11,40 @@ package chinesecheckers;
 import java.util.*;
 
 public class MoveSequence {
-	
+ 
 //Fields
-	private ArrayList<Hole> moves;
-	private final Hole ENDPOINT;
+ private ArrayList<Hole> moves;
+ private final Hole ENDPOINT;
 
-	//Constructors
-	public MoveSequence(){
-		moves = new ArrayList<Hole>();
-		ENDPOINT = new Hole(0,12);
-	}
-	
+ //Constructors
+ public MoveSequence(){
+  moves = new ArrayList<Hole>();
+  ENDPOINT = new Hole(0,12);
+ }
+ 
 //Methods
 
-	public void add(Hole h){
-		moves.add(h);
-	}
-	
-	public int size(){
-		return moves.size();
-	}
-	
-	public Hole get(int n){
-		return moves.get(n);
-	}
-	
-	//Returns distance traversed by move,
-	public int getDistance(Marble m){
-		if(Math.abs(moves.get(0).getRow() - m.getRow()) == 1 || Math.abs(moves.get(0).getCol() - m.getCol()) == 1) 
-			return 1;
-		else
-			return moves.size() * 2;
-	}
-	
-	public int distanceFromEnd(){
-		return Math.max(Math.abs(moves.get(moves.size()-1).getRow()-ENDPOINT.getRow()), Math.abs(moves.get(moves.size()-1).getCol()-ENDPOINT.getCol()));
-	}
+ public void add(Hole h){
+  moves.add(h);
+ }
+ 
+ public int size(){
+  return moves.size();
+ }
+ 
+ public Hole get(int n){
+  return moves.get(n);
+ }
+ 
+ //Returns distance traversed by move,
+ public int getDistance(Marble m){
+  if(Math.abs(moves.get(0).getRow() - m.getRow()) == 1 || Math.abs(moves.get(0).getCol() - m.getCol()) == 1) 
+   return 1;
+  else
+   return moves.size() * 2;
+ }
+ 
+ public int distanceFromEnd(){
+  return Math.max(Math.abs(moves.get(moves.size()-1).getRow()-ENDPOINT.getRow()), Math.abs(moves.get(moves.size()-1).getCol()-ENDPOINT.getCol()));
+ }
 }
